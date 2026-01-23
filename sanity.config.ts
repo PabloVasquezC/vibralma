eb6f378845e4dd01e337cbb262c5b028fc1e7610
pablo <vascor.pablo@gmail.com>
2026-01-23 12:55:01 -0300

    fixing sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,13 +8,16 @@ import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
+
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
 
 export default defineConfig({
-  basePath: '/studio',
+  basePath: isProduction ? undefined : '/studio',
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
--- sanity/env.ts
+++ sanity/env.ts
@@ -1,20 +1,11 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-01-22'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+export const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'ak47ihpr'
 
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-  if (v === undefined) {
-    throw new Error(errorMessage)
-  }
-
-  return v
-}
+export const token =
+  process.env.SANITY_API_WRITE_TOKEN || 'skGA3uJJK1QL5Rl1JwTWFbGYAyus9HA4vxi9DKeALOCQP7q9Rc4uSZMg2IsHU9U9PTc1MIMEQiAdsJjinxVJbwn6ZtFkALxthhsF3GBZZw7cbllvZGpc7VoiDNyrasdO2oGXWnKnJYGML22duufnRAdKkAM8pIfWHUhcC19vYZdPf6N1Aqwv'
