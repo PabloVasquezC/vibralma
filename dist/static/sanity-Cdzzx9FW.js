88640573a13cb7e69f0d9881fb21100fd8a52611
pablo <vascor.pablo@gmail.com>
2026-01-23 13:09:02 -0300

    fixing structure.ts

---
+++ dist/static/sanity-Cdzzx9FW.js
@@ -0,0 +1,1 @@
+import{r as t,s as e}from"./sanity.config-B3Is7B-Z.js";t(document.getElementById("sanity"),e,{reactStrictMode:!1,basePath:"/"});
--- sanity.config.ts
+++ sanity.config.ts
@@ -6,13 +6,12 @@ import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
-const isProduction = process.env.NODE_ENV === 'production'
-
-
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
   basePath: isProduction ? undefined : '/studio',
